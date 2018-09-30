c99b835aea8485ebce138b61ef220cd1d13b8e0f
raahishk <33830489+raahishk@users.noreply.github.com>
2018-09-30 00:02:58 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
 {
     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
      function(tabs){
-        if(tabs[0].url == "https://collab.its.virginia.edu/portal"){
+        if(tabs[0].url.substring(0,32) == "https://collab.its.virginia.edu/"){
           var newURL = "https://www.youtube.com/watch?v=-ncIVUXZla8";
           chrome.tabs.create({ url: newURL });
         }
--- content.js
+++ content.js
@@ -1,1 +1,1 @@
-alert("You are on UVACollab")
+alert("You are on UVACollab. This is just a temporary popup to show that content.js is operational on the Collab URL")
