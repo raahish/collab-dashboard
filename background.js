e77e586270d2f7126b77728fcb6bbfb805c34ee5
raahishk <33830489+raahishk@users.noreply.github.com>
2018-10-05 11:40:30 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
      function(tabs){
         if(tabs[0].url.substring(0,32) == "https://collab.its.virginia.edu/"){
-          var newURL = "https://www.youtube.com/watch?v=-ncIVUXZla8";
+          var newURL = "index2.html";
           chrome.tabs.create({ url: newURL });
         }
     });
---
+++ feedcontent.js
@@ -0,0 +1,35 @@
+function replace() {
+    $('#Assignment1_Name').html('Essay 5');
+    $('#Assignment1_Class').html('ENWR 1134');
+    $('#Assignment1_Due').html('29th September');
+    $('#Assignment1_Posted').html('29th September');
+    $('#Assignment1_URL').attr('href', 'https://www.google.com');
+
+    $('#Assignment2_Name').html('Prelab Experiment 2');
+    $('#Assignment2_Class').html('CHEM 1611');
+    $('#Assignment2_Due').html('29th September');
+    $('#Assignment2_Posted').html('29th September');
+    $('#Assignment2_URL').attr('href', 'https://www.google.com');
+
+    $('#Assignment3_Name').html('Homework 4');
+    $('#Assignment3_Class').html('ENGR 1624');
+    $('#Assignment3_Due').html('29th September');
+    $('#Assignment3_Posted').html('29th September');
+    $('#Assignment3_URL').attr('href', 'https://www.google.com');
+
+    $('#Announcement1_Subject').html('Information about Next Class');
+    $('#Announcement1_Class').html('ENGR 1624');
+    $('#Announcement1_Description').html('Hi Engineers, did you ever wish you ever had your own robot army? Youre in luck well begin learning how to program humanoid "Nao" robots tomorrow! This is a really cool opportunity...');
+    $('#Announcement1_URL').attr('href', 'https://www.google.com');
+
+    $('#Announcement2_Subject').html('Information about Next Class');
+    $('#Announcement2_Class').html('ENGR 1624');
+    $('#Announcement2_Description').html('Hi Engineers, did you ever wish you ever had your own robot army? Youre in luck well begin learning how to program humanoid "Nao" robots tomorrow! This is a really cool opportunity...');
+    $('#Announcement2_URL').attr('href', 'https://www.google.com');
+
+    $('#Announcement3_Subject').html('Information about Next Class');
+    $('#Announcement3_Class').html('ENGR 1624');
+    $('#Announcement3_Description').html('Hi Engineers, did you ever wish you ever had your own robot army? Youre in luck well begin learning how to program humanoid "Nao" robots tomorrow! This is a really cool opportunity...');
+    $('#Announcement3_URL').attr('href', 'https://www.google.com');
+}
+window.onload = replace;
